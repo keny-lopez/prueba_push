72800aa6e6871c291dad0ea00fa357b56fe77bf0
Keny <kenylopez.analytic@gmail.com>
2026-08-28 14:33:00 -0600

    Otra prueba en la rama de Visual Studio Code

--- scripts/prueba.py
+++ scripts/prueba.py
@@ -1,2 +1,4 @@
 print("Esta es una nueva prueba para la Rama de Visual Studio Code")
 print("Esta es otra prueba para la Rama de Visual Studio Code")
+
+print("Esto es para comprobar si funciona una nueva actualización desde la rama")
